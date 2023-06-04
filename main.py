86602b38c72b06f9469adc20172e6de785c6f33e
joepdeeben <jldeeben@gmail.com>
2023-06-04 03:34:33 +0200

    selenium dingen verbeterd

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def upload():
     driver.implicitly_wait(5)
     time.sleep(3)
     driver.find_element(By.XPATH, "//input[@type='file']").send_keys(
-        r"C:\Users\j\PycharmProjects\vinylrecognition\albums\Queen.jpg")
+        r"C:\Users\j\PycharmProjects\vinylrecognition\albums\Beatles.jpg")
     driver.implicitly_wait(5)
     time.sleep(2)
     driver.find_element(By.CLASS_NAME, "wNPKTe").click()
@@ -35,8 +35,27 @@ def upload():
     driver.switch_to.window(driver.window_handles[0])
     driver.close()
 
-    time.sleep(20)
+    time.sleep(5)
+    driver.switch_to.window(driver.window_handles[0])
     driver.get("https://www.discogs.com/")
+    driver.find_element(By.ID, "onetrust-reject-all-handler").click()
+
+    time.sleep(2)
+    driver.find_element(By.ID, "search_q").send_keys(f"{record}")
+
+    time.sleep(2)
+    driver.find_element(By.ID, "do_site_search").click()
+
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, "card-release-title").click()
+
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, "toggle__34Lx").click()
+
+    time.sleep(2)
+    print(driver.find_elements(By.CLASS_NAME, "column_2XUUw")[1].text)
+
+
     time.sleep(50)
 
     driver.quit()
